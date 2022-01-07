3f49c91a1b0225fedefa9e63de92926d6d993a7a
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-01-07 17:31:16 -0300

    alteração Caso você queira alterar a mensagem padrão de qualquer jeito, é possível fazer utilizando JavaScript.

---
+++ 02-exemploComHTML5Validator/js/app.js
@@ -0,0 +1,4 @@
+var campo1 = document.getElementById('nome');
+//Seleciona o campo com a ID "nome"
+campo1.setCustomValidity('Preencha este campo corretamente.');
+//uso setCustomValidity para trocar a mensagem de erro dele.
